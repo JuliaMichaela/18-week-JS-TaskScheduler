8246d7a535a5806068502e19cad4093a43ff56ab
JuliaMichaela <juliaperez@yandex.ru>
2024-12-16 23:10:15 +0300

    creted function createTask

---
+++ assets/scripts/app.js
@@ -0,0 +1,16 @@
+const input = document.querySelector('#input');
+const btnAdd = document.querySelector('#btn_add');
+const tasks = document.querySelector('#tasks');
+const btnClear = document.querySelector('#btn_clear');
+
+
+
+function createTask () {
+    const valueInput = input.value;
+    console.log(valueInput)
+    const textTask = document.createElement('p');
+    textTask.textContent = valueInput;
+    tasks.appendChild(textTask);
+    valueInput = ''
+}
+btnAdd.addEventListener('click', createTask);
