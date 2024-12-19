24789dd1f9a64f368ed29aeadcdd298b47f179bf
JuliaMichaela <juliaperez@yandex.ru>
2024-12-19 22:49:59 +0300

    added class Todo

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,28 +1,52 @@
-const input = document.querySelector('#input');
-const btnAdd = document.querySelector('#btn_add');
-const tasks = document.querySelector('#tasks');
-const btnClear = document.querySelector('#btn_clear');
+// const input = document.querySelector('#input');
+// const btnAdd = document.querySelector('#btn_add');
+// const tasks = document.querySelector('#tasks');
+// const btnClear = document.querySelector('#btn_clear');
 
 
-document.addEventListener("DOMContentLoaded", function (event) {
-    let arrTasks = localStorage.getItem('arrTasks');
-    if (arrTasks.length != 0) {
-        tasks.value = arrTasks;
+// document.addEventListener("DOMContentLoaded", function (event) {
+//     let arrTasks = localStorage.getItem('arrTasks');
+//     if (arrTasks.length != 0) {
+//         tasks.value = arrTasks;
+//     }
+// })
+
+// function createTask () {
+//     const valueInput = input.value;
+//     console.log(valueInput)
+//     const textTask = document.createElement('p');
+//     textTask.textContent = valueInput;
+//     tasks.appendChild(textTask);
+
+//     valueInput = '';
+// }
+// btnAdd.addEventListener('click', createTask);
+
+// function clearList (){
+//     tasks.textContent= '';
+// }
+// btnClear.addEventListener('click', clearList );
+
+
+class ToDolist {
+    constructor () {
+        this.list = [];
+        this.container = document.querySelector('.container');
+        this.form = document.forms.formTodo;
+        this.init();
     }
-})
 
-function createTask () {
-    const valueInput = input.value;
-    console.log(valueInput)
-    const textTask = document.createElement('p');
-    textTask.textContent = valueInput;
-    tasks.appendChild(textTask);
+    init () {
+        this.form.addEventListener ("submit", (event) => {
+            this.addTodo(event);
+        });
+    }
 
-    valueInput = '';
+    addTodo (event) {
+        event.preventDefault();
+    }
+
+    render ()
 }
-btnAdd.addEventListener('click', createTask);
 
-function clearList (){
-    tasks.textContent= '';
-}
-btnClear.addEventListener('click', clearList );
+new ToDolist();
