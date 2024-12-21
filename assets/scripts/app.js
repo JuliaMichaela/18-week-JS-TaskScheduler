a96480fb0f83bade551b18b36edf2be33813cb12
JuliaMichaela <juliaperez@yandex.ru>
2024-12-21 23:40:30 +0300

    updated

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -30,7 +30,7 @@
 
 class ToDolist {
     constructor () {
-        this.list = [];
+        this.list = localStorage.getItem("taskArr")? JSON.parse(localStorage.getItem("taskArr")): [];
         this.container = document.querySelector('.container');
         this.form = document.forms.formTodo;
         this.init();
@@ -40,13 +40,62 @@ class ToDolist {
         this.form.addEventListener ("submit", (event) => {
             this.addTodo(event);
         });
+        this.render();
     }
 
     addTodo (event) {
         event.preventDefault();
+        const {task} = this.form;
+        if (task.value === "") alert ("Please enter a task");
+        this.list.push(task.value);
+        const newList = JSON.stringify(this.list);
+        localStorage.setItem("taskArr", newList);
+        this.render();
+        task.value = "";
     }
 
-    render ()
+    render () {
+        if (this.list.length === 0) {
+            this.container.innerHTML = "<h2>No todos</h2>";
+            return
+        }
+        this.container.innerHTML = "";
+        this.list.forEach((task) => {
+            const li = document.createElement ("li");
+            li.innerText = task;
+            this.container.appendChild(li);
+        });
+    }
 }
 
-new ToDolist();
+// new ToDolist();
+
+const container = document.querySelector('.container');
+const form = document.forms.formTodo;
+const {task} = form;
+const list = localStorage.getItem("taskArr")
+? JSON.parse(localStorage.getItem("taskArr"))
+: [];
+
+form.addEventListener ("submit", (event) => {
+    event.preventDefault();
+    if (task.value === "") alert ("Please enter a task");
+        list.push(task.value);
+        const newList = JSON.stringify(this.list);
+        localStorage.setItem("taskArr", newList);
+        render();
+        task.value = "";
+});
+
+function render () {
+    if (list.length === 0) {
+        container.innerHTML = "<h2>No todos</h2>";
+        return
+    }
+    container.innerHTML = "";
+    list.forEach((task) => {
+        const li = document.createElement ("li");
+        li.innerText = task;
+        container.appendChild(li);
+    });
+}
